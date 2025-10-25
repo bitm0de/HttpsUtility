c819166b2879e49d07d12f7796be5a176d03299f
Troy Garner <troy.garner@tbdenterprises.ca>
2025-10-25 17:00:29 -0600

    chore: Updated AssemblyInfo.cs

--- HttpsUtility/Properties/AssemblyInfo.cs
+++ HttpsUtility/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@
 [assembly: AssemblyTitle("HttpsUtility")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HttpsUtility")]
-[assembly: AssemblyCopyright("Copyright © Troy Garner 2019")]
+[assembly: AssemblyCopyright("Copyright © Troy Garner 2025")]
 [assembly: AssemblyVersion("2.0.0")]
 
